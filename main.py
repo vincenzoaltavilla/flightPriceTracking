196d79e785baa71cae649ffcbd0cce67b578e4e4
vincenzoaltavilla <vincenzoaltavilla.va@gmail.com>
2024-04-08 15:34:29 +0200

    add following days

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ sheet_name = airportFrom+'-'+airportTo
 excel_file = sheet_name + '.xlsx'
 
 for dateOfFlight in dateOfFlights:
-
     info = get_flight_info("https://www.ryanair.com/it/it/trip/flights/select?adults=" + nOfPersons + "&teens=0&children=0&infants=0&dateOut=" + dateOfFlight + "&dateIn=&isConnectedFlight=false&discount=0&promoCode=&isReturn=false&originIata=" + airportFrom + "&destinationIata=" + airportTo + "&tpAdults=" + nOfPersons + "&tpTeens=0&tpChildren=0&tpInfants=0&tpStartDate=" + dateOfFlight + "&tpEndDate=&tpDiscount=0&tpPromoCode=&tpOriginIata=" + airportFrom + "&tpDestinationIata=" + airportTo + "")
 
     if info != 'No flights':

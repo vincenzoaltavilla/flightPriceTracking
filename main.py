9e7953405ec7f5819be97f1214477cdc34bfab88
vincenzoaltavilla <vincenzoaltavilla.va@gmail.com>
2024-04-10 12:51:15 +0200

    run=update

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ excel_file = sheet_name + '.xlsx'
 
 for dateOfFlight in dateOfFlights:
     info = get_flight_info("https://www.ryanair.com/it/it/trip/flights/select?adults=" + nOfPersons + "&teens=0&children=0&infants=0&dateOut=" + dateOfFlight + "&dateIn=&isConnectedFlight=false&discount=0&promoCode=&isReturn=false&originIata=" + airportFrom + "&destinationIata=" + airportTo + "&tpAdults=" + nOfPersons + "&tpTeens=0&tpChildren=0&tpInfants=0&tpStartDate=" + dateOfFlight + "&tpEndDate=&tpDiscount=0&tpPromoCode=&tpOriginIata=" + airportFrom + "&tpDestinationIata=" + airportTo + "")
-
+    print("Analizzando voli in data " + dateOfFlight)
     if info != 'No flights':
         for flight in info:
             col.append(dateOfFlight+", "+flight['Departure']+"-"+flight['Arrival'])

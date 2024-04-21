002e6224d49cff9f2b0c3361472b575d6f1d1e9b
vince <vincenzoaltavilla.va@gmail.com>
2024-04-21 16:10:44 +0200

    matched front-end w back.end

---
+++ get_n_plot_prices.py
@@ -0,0 +1,64 @@
+from get_flight_info import *
+from format import *
+from gui import *
+from datetime import date
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf
+import keyboard
+
+
+def get_prices(airport_from, airport_to, date_of_flights, n_of_persons):
+    sheet_name = airport_from + '-' + airport_to
+    excel_file = n_of_persons + '-' + sheet_name + "-" + ",".join(date_of_flights) + ".xlsx"
+
+    column_complete_flight, prices = [], []
+
+    for date_of_flight in date_of_flights:
+        URL = (f"https://www.ryanair.com/it/it/trip/flights/select?adults={n_of_persons}&teens=0&children=0&infants=0"
+               f"&dateOut={date_of_flight}&dateIn=&isConnectedFlight=false&discount=0&promoCode=&isReturn=false"
+               f"&originIata={airport_from}&destinationIata={airport_to}&tpAdults={n_of_persons}&tpTeens=0&tpChildren=0&"
+               f"tpInfants=0&tpStartDate={date_of_flight}&tpEndDate=&tpDiscount=0&tpPromoCode=&tpOriginIata={airport_from}&"
+               f"tpDestinationIata={airport_to}")
+        # print(URL)
+
+        info = get_flight_info(URL)
+
+        if info:
+            for flight in info:
+                column_complete_flight.append(date_of_flight + ", " + flight['Departure'] + "-" + flight['Arrival'])
+                prices.append(flight['Price'])
+                # print(flight)
+
+    if os.path.exists(excel_file):
+        df = pd.read_excel(excel_file, sheet_name=sheet_name, index_col=0)
+    elif column_complete_flight:
+        df = pd.DataFrame(index=["Prezzi al " + str(date.today())], columns=column_complete_flight)
+
+    if prices and len(prices) == len(column_complete_flight):
+        df.loc["Prezzi al " + str(date.today())] = [float(price.replace(' €', '').replace(',', '.')) for price in prices]
+        df.to_excel(excel_file, sheet_name=sheet_name)
+        adapt_columns(excel_file, sheet_name)
+
+        print("\n\n" + df.to_string())
+
+        x = df.index.values.tolist()
+        y = df.values.tolist()
+
+        plt.xticks(range(len(x)), x)
+        plt.plot(y, "-o")
+        plt.ylabel("€")
+        plt.grid()
+
+        plt.legend(column_complete_flight, bbox_to_anchor=(0.5, 1.2), loc='upper center', ncol=3)
+        plt.tight_layout()
+
+        plt.gcf().autofmt_xdate()
+        plt.show()
+        plt.draw()
+
+
+if __name__ == "__main__":
+    app = Home()
+    app.mainloop()
--- gui.py
+++ gui.py
@@ -4,8 +4,12 @@ from tkinter import *
 from tkcalendar import DateEntry
 from tkinter import messagebox
 from datetime import date
-from routes import *
+from routes import routes
+from alias import alias
+from get_n_plot_prices import get_prices
 from PIL import Image, ImageTk
+from time import sleep
+import babel.numbers
 
 
 def validate_input(new_text):
@@ -16,13 +20,17 @@ def validate_input(new_text):
 
 
 # noinspection PyUnusedLocal
+def execute_get_prices(finestra_secondaria, selected_airport_from, selected_airport_to, selected_dates,
+                       selected_n_of_persons):
+    get_prices(alias[selected_airport_from], alias[selected_airport_to], selected_dates, selected_n_of_persons)
+    finestra_secondaria.destroy()  # Distruggi la finestra secondaria dopo l'esecuzione di get_prices
+
+
 class Home(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Ryanair Flight Prices")
         self.iconbitmap("icon.ico")
-
-        self.selected_dates = []
 
         # window sizes and position
         window_width = 700
@@ -45,7 +53,7 @@ class Home(tk.Tk):
 
         # Airport from
         self.label_1 = tk.Label(self.frame_gui, text="Aeroporto di partenza:", font=("Arial", 12), bg="#073693",
-                                fg = "white")
+                                fg="white")
         self.label_1.grid(row=1, column=0, padx=10, pady=10, sticky="e")
         airport_from = list(routes)
         self.var_1 = tk.StringVar(value=airport_from[184])
@@ -54,7 +62,7 @@ class Home(tk.Tk):
         self.menu_airport_from.grid(row=1, column=1, padx=10, pady=10)
 
         # Airport to
-        self.label_2 = tk.Label(self.frame_gui, text="Aeroporto di arrivo:", font=("Arial", 12), bg="#073693", fg = "white")
+        self.label_2 = tk.Label(self.frame_gui, text="Aeroporto di arrivo:", font=("Arial", 12), bg="#073693", fg="white")
         self.label_2.grid(row=2, column=0, padx=10, pady=10, sticky="e")
         self.airport_to = routes
         self.var_2 = tk.StringVar(value=self.airport_to[airport_from[24]])
@@ -64,8 +72,9 @@ class Home(tk.Tk):
         self.menu_airport_from.bind("<<ComboboxSelected>>", self.update_airport_to_menu)
         self.update_airport_to_menu(None)
 
+        self.selected_dates = []
         # Calendar
-        self.label_calendar = tk.Label(self.frame_gui, text="Date:", font=("Arial", 12), bg="#073693", fg = "white")
+        self.label_calendar = tk.Label(self.frame_gui, text="Date:", font=("Arial", 12), bg="#073693", fg="white")
         self.label_calendar.grid(row=3, column=0, padx=10, pady=10, sticky="e")
         self.calendar = DateEntry(self.frame_gui, width=30, background="#cdab2a", foreground="#073693", borderwidth=2,
                                   date_pattern="yyyy-mm-dd", state="readonly", font=("Arial", 12))
@@ -77,15 +86,15 @@ class Home(tk.Tk):
         self.add_date_button.grid(row=3, column=2, padx=10, pady=10)
 
         # Selected dates list
-        self.label_selected_dates = tk.Label(self.frame_gui, text="Date selezionate:", font=("Arial", 12), bg = "#073693",
-                                             fg = "white")
+        self.label_selected_dates = tk.Label(self.frame_gui, text="Date selezionate:", font=("Arial", 12), bg="#073693",
+                                             fg="white")
         self.label_selected_dates.grid(row=3, column=0, padx=10, pady=10, sticky="e")
         self.scrollbar = Scrollbar(self.frame_gui, orient=VERTICAL)
         self.dates_list = Listbox(self.frame_gui, width=30, height=5, font=("Arial", 12), borderwidth=2,
                                   yscrollcommand=self.scrollbar.set)
         self.scrollbar.config(command=self.dates_list.yview)
         self.dates_list.grid(row=4, column=1, padx=10, pady=10, columnspan=1, sticky="nsw")
-        self.scrollbar.grid(row=4, column=1, padx=10, pady=10,  sticky="nse")
+        self.scrollbar.grid(row=4, column=1, padx=10, pady=10, sticky="nse")
 
         # Delete date button
         self.delete_date_button = tk.Button(self.frame_gui, text="Cancella data", command=self.delete_date,
@@ -95,7 +104,7 @@ class Home(tk.Tk):
 
         # Label number of persons
         self.label_number_of_persons = tk.Label(self.frame_gui, text="Numero di persone:", font=("Arial", 12),
-                                                bg = "#073693", fg = "white")
+                                                bg="#073693", fg="white")
         self.label_number_of_persons.grid(row=5, column=0, padx=10, pady=10, sticky="e")
 
         # Spinbox for number of persons
@@ -197,7 +206,25 @@ class Home(tk.Tk):
             messagebox.showerror("Errore", "Il campo del numero di persone non può essere vuoto.")
             return
 
-        print("Aeroporto di partenza:", selected_airport_from)
-        print("Aeroporto di arrivo:", selected_airport_to)
-        print("Date Selezionate:", selected_dates)
-        print("Numero di persone:", selected_n_of_persons)
+        searching_window = tk.Toplevel(self)
+        searching_window.title("Ricercando prezzi...")
+        searching_window.iconbitmap("icon.ico")
+        searching_window.resizable(False, False)
+
+        # window sizes and position
+        window_width = 350
+        window_height = 260
+        x_position = (searching_window.winfo_screenwidth() - window_width) // 2
+        y_position = (searching_window.winfo_screenheight() - window_height) // 2
+        searching_window.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
+        # Aggiungi altri widget o contenuti alla finestra secondaria
+        label = tk.Label(searching_window, text="Questa è la finestra secondaria")
+        label.pack(padx=10, pady=10)
+
+        # Aggiorna la GUI prima di continuare con l'esecuzione
+        self.update()
+
+        # Esegui la funzione get_prices dopo 1 millisecondo
+        searching_window.after(1, lambda: execute_get_prices(searching_window, selected_airport_from,
+                                                                     selected_airport_to, selected_dates,
+                                                                     selected_n_of_persons))
